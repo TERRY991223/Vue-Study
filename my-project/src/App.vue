ae9f57df8ec683d4fc3b540a620e5f9d05a4b0cc
shinhyesoo <chfhrl99@naver.com>
2025-02-04 17:12:29 +0900

    project1

---
+++ my-project/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref, onMounted} from 'vue';
+import ChildComp from './components/ChildComp.vue';
+
+let count = ref(0); //상태변수 초기값 (state)
+let title = "Hello Vue";
+
+onMounted(()=> {
+  console.log('mounted');
+})
+
+const increaseCount = () => {
+  count.value++;
+}
+</script>
+
+<template>
+ <div>
+  <h1>App</h1>
+  <button @click="increaseCount">count++</button>
+  <p>{{  count }}</p>
+  <ChildComp color="red"/>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ my-project/src/components/ChildComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Child Component</h2>
+        <p :style="{color : color}">{{ color }}</p>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    color:String,
+})
+console.log(props.color);
+</script>
+<style>
+    
+</style>
